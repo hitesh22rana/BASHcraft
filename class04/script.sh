aa877649bb70d14a47bfb136d10b43cd1a007028
hitesh <hitesh22rana@gmail.com>
2023-09-01 20:18:03 +0530

    chore: class04 If statements

---
+++ class04/script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Lesson-4: If statements
+
+name="Hitesh"
+age="21"
+
+if [ $num < 200 ]
+then
+    echo "Number is less than 200"
+else
+    echo "Number is greater than or equal to 200"
+fi
