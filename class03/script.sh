449f49fb2eeee3f7a71eb0542aaac8dbe080e620
hitesh <hitesh22rana@gmail.com>
2023-09-01 20:17:19 +0530

    chore: class03 Math with Bash

---
+++ class03/script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Lesson-3: Math with Bash
+
+num1=20
+num2=11
+
+echo "num1 = $num1 and num2 = $num2"
+echo "Addition: $((num1 + num2))"
+echo "Subtraction: $((num1 - num2))"
+echo "Multiplication: $((num1 * num2))"
+echo "Division: $((num1 / num2))"
+echo "Modulus: $((num1 % num2))"
+echo "Exponent: $((num1 ** num2))"
+echo "Increment of type ++val: $((++num1))"
+echo "Decrement of type --val: $((--num2))"
+echo "Increment of type val++: $((num1++))"
+echo "Decrement of type val--: $((num2--))"
+echo "num1 = $num1 and num2 = $num2"
